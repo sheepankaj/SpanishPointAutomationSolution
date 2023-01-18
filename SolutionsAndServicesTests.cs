2cee6ca6fa236a537e9f9a31eab60ad48088dec7
Pankaj Kumar Shee <44091458+sheepankaj@users.noreply.github.com>
2023-01-18 23:12:20 +0000

    Adding UI Tests file

---
+++ SolutionsAndServicesTests.cs
@@ -0,0 +1,20 @@
+﻿using SpanishPointAutomationSolution.Framework.Common;
+using SpanishPointAutomationSolution.Framework.POM;
+using Xunit;
+
+namespace SpanishPointAutomationSolution;
+
+[Collection("Solutions And Services Tab- Modern Work Tests")]
+public class SolutionsAndServicesTests : TestBase
+{
+    /// <summary>
+    /// Navigate to Modern Work and validate texts in the Employee Experience tab
+    /// </summary>
+    [Fact]
+    public void ValidateEmployeeExperienceTabTexts()
+    {
+        new SolutionsAndServicesTabPage(this, true)
+            .SelectModernWorkOptionFromTheDropDownListOfSolutionAndServicesTab()
+            .ClickEmployeeExperienceTabAndValidateText();
+    }
+}
